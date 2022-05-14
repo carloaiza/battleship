3b6e1e35e87449a26864b6b0a2c22008cf85ddf0
Carlos Loaiza <carloaiza@umanizales.edu.co>
2022-05-14 10:42:25 -0500

    Model ListDE Ship Distribution

--- controller/list_de_controller.py
+++ controller/list_de_controller.py
@@ -1,6 +1,7 @@
 from service.list_de_service import ListDEService
 from flask import json, Response,jsonify,Blueprint, request
 from util.util_encoder import UtilEncoder
+from model.ship import Ship
 
 app_list_de = Blueprint('app_list_de',__name__)
 list_de_service = ListDEService()
@@ -9,3 +10,8 @@ list_de_service = ListDEService()
 def list():
     return Response(status=200,mimetype="application/json",
                     response=json.dumps(list_de_service.list(),cls=UtilEncoder))
+
+@app_list_de.route('/listde',methods=['POST'])
+def create():
+    data = request.json
+    return list_de_service.add(Ship(data,list_de_service.list_de.count +1))
